0ff9c56a9e8057a85f268721cd183fc1bbd93ffb
Bassem Makarem <bassem@Bassems-MacBook-Pro.local>
2020-02-29 22:39:22 +0200

    save new connection if both users already exists

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/info/:hash', require('./info'));
+router.post('/info/:key', require('./info'));
 router.post('/save', require('./save'));
 
 module.exports = router;
